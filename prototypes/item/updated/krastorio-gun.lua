3709363b145673888115f0dd17041e3eb35442ee
Mathieu-S <mathieusartorio@gmail.com>
2020-03-24 19:25:36 +0100

    * ADD: Compatibility to Krastorio 2

---
+++ prototypes/item/updated/krastorio-gun.lua
@@ -0,0 +1,3 @@
+-- Update the entity advanced-tank-machine-gun
+data.raw["gun"]["advanced-tank-machine-gun"].attack_parameters.ammo_category = "turret-bullet"
+data.raw["gun"]["advanced-tank-machine-gun"].icon = "__base__/graphics/icons/gun-turret.png"
