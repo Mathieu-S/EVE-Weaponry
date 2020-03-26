3580652677ea2596fbce3436d84a83cdea9c5967
kurosnow <ashenswift@gmail.com>
2020-03-26 16:07:23 -0400

    Add files via upload

---
+++ prototypes/entity/updated/rampantturret.lua
@@ -0,0 +1,5 @@
+data.raw["ammo-turret"]["gun-ammo-turret-rampant-arsenal"].attack_parameters.ammo_category = "turret-bullet"
+
+if settings.startup["everampantrifle"].value then
+	data.raw["ammo-turret"]["rifle-ammo-turret-rampant-arsenal"].attack_parameters.ammo_category = "turret-bullet"
+end
