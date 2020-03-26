2094d007801aacea10912954240a76d9419dfb23
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2020-03-26 11:37:37 +0100

    * UPD: Comment for modularturret.lua

--- prototypes/entity/updated/modularturret.lua
+++ prototypes/entity/updated/modularturret.lua
@@ -1,4 +1,4 @@
--- Update the entity gun-turret
+-- Update the entities for Modular Turrets
 data.raw["ammo-turret"]["w93-gatling-turret"].attack_parameters.ammo_category = "turret-bullet"
 data.raw["ammo-turret"]["w93-gatling-turret2"].attack_parameters.ammo_category = "turret-bullet"
 data.raw["ammo-turret"]["w93-hmg-turret"].attack_parameters.ammo_category = "turret-bullet"
