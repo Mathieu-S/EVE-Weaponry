a2c30ae85fd3dd1f6cbcfdd4368ea4f97ecabb07
kurosnow <ashenswift@gmail.com>
2020-03-26 03:14:21 -0400

    Add files via upload

---
+++ prototypes/entity/updated/modularturret.lua
@@ -0,0 +1,5 @@
+-- Update the entity gun-turret
+data.raw["ammo-turret"]["w93-gatling-turret"].attack_parameters.ammo_category = "turret-bullet"
+data.raw["ammo-turret"]["w93-gatling-turret2"].attack_parameters.ammo_category = "turret-bullet"
+data.raw["ammo-turret"]["w93-hmg-turret"].attack_parameters.ammo_category = "turret-bullet"
+data.raw["ammo-turret"]["w93-hmg-turret2"].attack_parameters.ammo_category = "turret-bullet"
