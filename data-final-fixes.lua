bea8da1d598e0b3434f8256bc61f9554bbd25590
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2020-03-26 23:34:09 +0100

    * UPD: Rename require in data-final-fixes

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,7 +1,7 @@
 if mods["scattergun_turret"] then	
-	require("prototypes.entity.updated.modularturret")
 end
 if mods["RampantArsenal"] then
 	require("prototypes.entity.updated.rampantturret")
 	require("prototypes.item.updated.rampantminigun")
+	require("prototypes.entity.updated.modular-turret")
 end
