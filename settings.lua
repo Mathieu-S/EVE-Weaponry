921ea2b03fc3a91f65767702b3a9432b53c96fb3
kurosnow <ashenswift@gmail.com>
2020-03-26 16:12:19 -0400

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -4,14 +4,14 @@ data:extend({
 		type = "bool-setting",
 		name = "everampantrifle",
 		setting_type = "startup",
-		default_value = false,
+		default_value = true,
 		order = "a1"
 	},
 	{
 		type = "bool-setting",
 		name = "everampantminigun",
 		setting_type = "startup",
-		default_value = false,
+		default_value = true,
 		order = "a2"
 	}
 })
