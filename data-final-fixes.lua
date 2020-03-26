7bb85eada34dc8956613142906430527ebd6828c
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2020-03-26 23:41:12 +0100

    * UPD: Move Rampant Arsenal loading file in data-updates

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,7 +1,3 @@
 if mods["scattergun_turret"] then	
-end
-if mods["RampantArsenal"] then
-	require("prototypes.entity.updated.rampantturret")
-	require("prototypes.item.updated.rampantminigun")
 	require("prototypes.entity.updated.modular-turret")
 end
--- data-updates.lua
+++ data-updates.lua
@@ -5,7 +5,11 @@ require("prototypes.technology.updated.military-upgrades")
 require("prototypes.technology.updated.technology")
 
 -- Mods requires :
-
 if mods["Krastorio2"] then	
 	require("prototypes.item.updated.krastorio-gun")
+end
+
+if mods["RampantArsenal"] then
+	require("prototypes.entity.updated.rampant-turret")
+	require("prototypes.item.updated.rampant-minigun")
 end
