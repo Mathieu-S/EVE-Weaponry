3541d535e42cb895378c9b8226a4b8e1c8ecd733
kurosnow <ashenswift@gmail.com>
2020-03-26 03:13:28 -0400

    Update data-updates.lua

--- data-updates.lua
+++ data-updates.lua
@@ -6,3 +6,6 @@ require("prototypes.technology.updated.technology")
 if mods["Krastorio2"] then	
 	require("prototypes.item.updated.krastorio-gun")
 end
+if mods["scattergun_turret"] then	
+	require("prototypes.entity.updated.modularturret")
+end
