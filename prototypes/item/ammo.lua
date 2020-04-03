7ead2fbd9509749ba74a70ac2ace8c3a9f10a9ec
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2020-04-03 12:12:13 +0200

    * FIX: Indentation

--- prototypes/item/ammo.lua
+++ prototypes/item/ammo.lua
@@ -5,7 +5,7 @@ data:extend(
     name = "small-carbonized-lead",
     icon = "__eve-weaponry__/graphics/ammo/small_carbonized_lead.png",
     icon_size = 64,
-	icon_mipmaps = 4,
+	  icon_mipmaps = 4,
     ammo_type =
     {
       category = "turret-bullet",
@@ -44,7 +44,7 @@ data:extend(
     name = "small-depleted-uranium",
     icon = "__eve-weaponry__/graphics/ammo/small_depleted_uranium.png",
     icon_size = 64,
-	icon_mipmaps = 4,
+  	icon_mipmaps = 4,
     ammo_type =
     {
       category = "turret-bullet",
@@ -87,7 +87,7 @@ data:extend(
     name = "small-emp",
     icon = "__eve-weaponry__/graphics/ammo/small_emp.png",
     icon_size = 64,
-	icon_mipmaps = 4,
+	  icon_mipmaps = 4,
     ammo_type =
     {
       category = "turret-bullet",
@@ -134,7 +134,7 @@ data:extend(
     name = "small-fusion",
     icon = "__eve-weaponry__/graphics/ammo/small_fusion.png",
     icon_size = 64,
-	icon_mipmaps = 4,
+  	icon_mipmaps = 4,
     ammo_type =
     {
       category = "turret-bullet",
@@ -177,7 +177,7 @@ data:extend(
     name = "small-nuclear",
     icon = "__eve-weaponry__/graphics/ammo/small_nuclear.png",
     icon_size = 64,
-	icon_mipmaps = 4,
+  	icon_mipmaps = 4,
     ammo_type =
     {
       category = "turret-bullet",
@@ -216,7 +216,7 @@ data:extend(
     name = "small-phased-plasma",
     icon = "__eve-weaponry__/graphics/ammo/small_phased_plasma.png",
     icon_size = 64,
-	icon_mipmaps = 4,
+  	icon_mipmaps = 4,
     ammo_type =
     {
       category = "turret-bullet",
@@ -263,7 +263,7 @@ data:extend(
     name = "small-proton",
     icon = "__eve-weaponry__/graphics/ammo/small_proton.png",
     icon_size = 64,
-	icon_mipmaps = 4,
+  	icon_mipmaps = 4,
     ammo_type =
     {
       category = "turret-bullet",
@@ -310,7 +310,7 @@ data:extend(
     name = "small-titanium-sabot",
     icon = "__eve-weaponry__/graphics/ammo/small_titanium_sabot.png",
     icon_size = 64,
-	icon_mipmaps = 4,
+  	icon_mipmaps = 4,
     ammo_type =
     {
       category = "turret-bullet",
